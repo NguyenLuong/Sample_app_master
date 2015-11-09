a084bf9edd82b36a34fc67775024aa45c7a7ab8b
NguyenLuong <khacluong.hedspi@gmail.com>
2015-11-09 22:29:11 +0700

    Finish user edit, update, index, and destroy action

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "nguyenluong",
+             email: "freedom@gmail.com",
+             password:              "12345678",
+             password_confirmation: "12345678",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "freedom#{n+1}@gmail.com"
+  password = "12345678"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
